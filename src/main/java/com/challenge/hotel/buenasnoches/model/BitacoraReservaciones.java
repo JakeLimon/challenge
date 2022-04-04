444eb84f20d70e35350fb0b60d9147128dcdcda9
Jason Limon <jason.limon@globant.com>
2022-04-04 18:22:47 -0500

    I have created the first model and I am testing the map fot DB Postgresql because it is a new technologie for me i still learning more postgresql

---
+++ src/main/java/com/challenge/hotel/buenasnoches/model/BitacoraReservaciones.java
@@ -0,0 +1,32 @@
+package com.challenge.hotel.buenasnoches.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table( name = "bitacora_reservaciones")
+public class BitacoraReservaciones {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY )
+    private Integer id;
+
+    @Column( name = "hotel_name" )
+    private String hotelName;
+    @Column( name = "room_name")
+    private String roomName;
+    @Column( name = "guest_first_name" )
+    private String guestFirstName;
+    @Column( name = "guest_last_name" )
+    private String guestLastName;
+    @Column( name = "guest_email" )
+    private String guestEmail;
+    @Column( name = "start_date")
+    private LocalDate startDate;
+    @Column( name = "end_date" )
+    private LocalDate endDate;
+
+}
